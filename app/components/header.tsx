f292daab960b116a01b220c7d2af8726e5ff5a21
Renan Braga <azhariel@gmail.com>
2024-09-10 14:41:21 -0300

    add header

---
+++ app/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+	return (
+		<header className='bg-white flex w-full items-center justify-between px-20 py-6'>
+			<img src='logo.svg' alt='Logo Resultados Digitais' className='h-10 w-auto' />
+			<h1 className='font-heading font-black text-heading-xs text-black'>Gerador de Cartão de Visita</h1>
+		</header>
+	);
+};
+
+export default Header;
