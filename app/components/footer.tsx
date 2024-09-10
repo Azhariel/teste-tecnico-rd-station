b1a41e710fba501e8f9d02f362bdadc8bef4a604
Renan Braga <azhariel@gmail.com>
2024-09-10 18:40:31 -0300

    add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='text-center text-gray-500 py-4 bg-gray-50 font-body'>
+			<p>
+				<a
+					href='https://legal.rdstation.com/pt/privacy-policy/'
+					target='_blank'
+					rel='noopener noreferrer'
+					className='underline font-semibold'
+				>
+					Política de Privacidade
+				</a>{' '}
+				© 2023 Resultados Digitais
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
