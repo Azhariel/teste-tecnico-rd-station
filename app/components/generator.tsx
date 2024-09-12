0a4b2701b703544b7eadc5aac5d4d1446df6c42a
Renan Braga <azhariel@gmail.com>
2024-09-12 02:01:03 -0300

    add logic to generator

--- app/components/generator.tsx
+++ app/components/generator.tsx
@@ -1,6 +1,57 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import Button from './button';
+
+interface FormValues {
+	name: string;
+	email: string;
+	phone: string;
+}
 
 const Generator = () => {
+	const [formValues, setFormValues] = useState<FormValues>({
+		name: '',
+		email: '',
+		phone: '',
+	});
+
+	const [errors, setErrors] = useState<{ name?: string; email?: string; phone?: string }>({});
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+		setFormValues({ ...formValues, [name]: value });
+		if (name === 'phone') event.target.value = formatPhone(value);
+		validateField(name, value);
+	};
+
+	const formatPhone = (value: string) => {
+		if (!value) return '';
+		value = value.replace(/\D/g, '');
+		value = value.replace(/(\d{2})(\d)/, '($1) $2');
+		value = value.replace(/(\d)(\d{4})$/, '$1-$2');
+		return value;
+	};
+
+	const validateField = (fieldName: string, value: string) => {
+		const newErrors = { ...errors };
+		switch (fieldName) {
+			case 'name':
+				newErrors.name = value.length < 2 ? 'Nome deve ter pelo menos 2 caracteres' : '';
+				break;
+			case 'email':
+				const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+				newErrors.email = !emailRegex.test(value) ? 'Email inválido' : '';
+				break;
+			case 'phone':
+				const phoneRegex = /^\(\d{2}\)\s\d\s\d{4}-\d{4}$/;
+				newErrors.phone = !phoneRegex.test(value) ? 'Telefone inválido' : '';
+				break;
+			default:
+				break;
+		}
+		setErrors(newErrors);
+	};
+
 	return (
 		<div className='flex flex-col flex-1 mx-8 gap-y-8 lg:mx-12 lg:gap-y-0 justify-center'>
 			<div className='flex flex-col items-center justify-center lg:mb-12'>
@@ -25,19 +76,23 @@ const Generator = () => {
 									id='name'
 									name='name'
 									placeholder='Nome completo'
-									className='placeholder:text-body-sm px-2 h-9'
+									onChange={handleChange}
+									className='placeholder:text-body-sm px-2 h-9 text-gray-100'
 								/>
+								{errors.name && <p className='text-body-sm text-highlight'>{errors.name}</p>}
 							</div>
 							<div className='flex flex-1 mb-2 justify-between lg:gap-12'>
 								<div className='flex flex-col w-2/5 lg:w-1/2'>
 									<label htmlFor='phone'>Telefone*</label>
 									<input
-										type='text'
+										type='tel'
 										id='phone'
 										name='phone'
 										placeholder='(00) 0 0000-0000'
-										className='placeholder:text-body-sm px-2 h-9'
+										onChange={handleChange}
+										className='placeholder:text-body-sm px-2 h-9 text-gray-100'
 									/>
+									{errors.phone && <p className='text-body-sm text-highlight'>{errors.phone}</p>}
 								</div>
 
 								<div className='flex flex-col w-2/5 lg:w-1/2'>
@@ -47,15 +102,15 @@ const Generator = () => {
 										id='email'
 										name='email'
 										placeholder='nome@email.com'
-										className='placeholder:text-body-sm px-2 h-9'
+										onChange={handleChange}
+										className='placeholder:text-body-sm px-2 h-9 text-gray-100'
 									/>
+									{errors.email && <p className='text-body-sm text-highlight'>{errors.email}</p>}
 								</div>
 							</div>
 						</div>
-						<button type='submit' className='bg-highlight px-4 py-2 w-full text-body-lg font-bold mt-8 text-gray-100'>
-							GERAR CARTÃO GRÁTIS
-						</button>
 					</form>
+					<Button onClick={() => console.log('works')}>{'GERAR CARTÃO GRÁTIS'}</Button>
 
 					<ul className='list-disc list-inside mt-8'>
 						<li>Ao preencher o formulário, concordo * em receber comunicações de acordo com meus interesses.</li>
