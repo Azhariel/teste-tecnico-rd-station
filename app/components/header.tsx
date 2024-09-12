9bb3f02c4aeaa667178398927eabffce882c88ec
Renan Braga <azhariel@gmail.com>
2024-09-12 13:48:16 -0300

    refactor header component to improve responsiveness and alignment

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header: React.FC = () => {
 	return (
-		<header className='bg-white flex w-full items-center justify-between px-20 py-6'>
+		<header className='bg-white flex w-full items-center justify-between px-2 lg:px-20 py-6'>
 			<img src='logo.svg' alt='Logo Resultados Digitais' className='h-10 w-auto' />
-			<h1 className='font-heading font-black text-heading-xs text-gray-100'>Gerador de Cartão de Visita</h1>
+			<h1 className='font-heading font-black lg:text-heading-xs text-right text-gray-100'>
+				Gerador de Cartão de Visita
+			</h1>
 		</header>
 	);
 };
